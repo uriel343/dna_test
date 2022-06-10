dbcff3641a961f802ceee3531f9f968e15b310d2
Uriel Ortiz <ortizurielr70@gmail.com>
2022-06-10 02:53:27 -0600

    Finished the endpoint to compare dna and the methods for insert registers into database were completed

--- src/controllers/readDNAController.js
+++ src/controllers/readDNAController.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const register = require("../models/registerModel")
+const register = require("../models/registerModel");
 
 async function verificatorDNA(req, res) {
   try {
-    let dnaArr = req.body.dna; 
+    let dnaArr = req.body.dna;
 
     if (!Array.isArray(dnaArr))
       return res.status(400).send({ message: "Please entry an array value" });
@@ -27,9 +27,10 @@ async function verificatorDNA(req, res) {
       }
 
       let verificator = contentArr.split("");
-      console.log(verificator)
+      console.log(verificator);
       for (let j = 0; j < verificator.length - 1; j++) {
         let letter = verificator[j];
+        if(letter !== 'A' || letter !== 'T' || letter !== 'C' || letter !== 'G') return res.status(404).send({ message: "The characters from dna are invalid, please entry valid values (A,T,C,G)" })
         if (letter == verificator[j + 1]) {
           counter++;
           if (counter === 4) {
@@ -42,55 +43,76 @@ async function verificatorDNA(req, res) {
         }
       }
 
-        if (result === "Mutant"){
-          caseMutant = verificator.join('')
-           registerMutant(caseMutant);
-        } else {
-          caseHuman = verificator.join('')
-           registerHuman(caseHuman);
-        }
+      if (result === "Mutant") {
+        caseMutant = verificator.join("");
+        registerMutant(caseMutant);
+      } else {
+        caseHuman = verificator.join("");
+        registerHuman(caseHuman);
+      }
     }
-    return res.status(200).send({ message: "OK " })
+    return res.status(200).send({ message: "OK " });
   } catch (error) {
     console.error(error);
   }
 }
 
- function registerHuman(human_dna){
-  let human = new register();
-  const type = 'HUMAN'
+function registerHuman(human_dna) {
   try {
-     register.findOne({dna_string:  human_dna}, (err, dnaFound) => {
-      if(err) return err
-      if(!dnaFound) {
-        human.type = type
-        human.dna_string = human_dna
+    let human = new register();
+    const type = "HUMAN";
+    register.findOne({ dna_string: human_dna }, (err, dnaFound) => {
+      if (err) return err;
+      if (!dnaFound) {
+        human.type = type;
+        human.dna_string = human_dna;
         human.save((err, dnaRegistered) => {
-          if(err) return err
-          if(!dnaRegistered) {console.log(dnaRegistered); return dnaRegistered}
-        })
+          if (err) return err;
+          if (!dnaRegistered) {
+            console.log(dnaRegistered);
+            return dnaRegistered;
+          }
+        });
       }
-    })
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-
 }
 
-function registerMutant(mutant_dna){
-  let mutant = new register();
-  const type = 'MUTANT'
+function registerMutant(mutant_dna) {
   try {
-     register.findOne({dna_string:  mutant_dna}, (err, dnaFound) => {
-      if(err) return err
-      if(!dnaFound) {
-        mutant.type = type
-        mutant.dna_string = mutant_dna
+    let mutant = new register();
+    const type = "MUTANT";
+    register.findOne({ dna_string: mutant_dna }, (err, dnaFound) => {
+      if (err) return err;
+      if (!dnaFound) {
+        mutant.type = type;
+        mutant.dna_string = mutant_dna;
         mutant.save((err, dnaRegistered) => {
-          if(err) return err
-          if(!dnaRegistered) console.log(dnaRegistered); return dnaRegistered
-        })
+          if (err) return err;
+          if (!dnaRegistered) console.log(dnaRegistered);
+          return dnaRegistered;
+        });
       }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function readStats(req, res) {
+  try {
+     register.countDocuments({type : "MUTANT"}, (err, numberOfMutants)=>{
+      if(err) return res.status(500).send({ message: "Server error" })
+        if(!numberOfMutants) return res.status(404).send({ message: "Cannot count the documents from mutants" })
+          register.countDocuments({type: "HUMAN"}, (err, numberOfHumans)=>{
+            if(err) return res.status(500).send({ message: "Server error" })
+              if(!numberOfHumans) return res.status(500).send({ message: "Cannot count the documents from humans" })
+                let totallystats = numberOfMutants + numberOfHumans
+                let percentageMutants = (numberOfMutants * 100)/totallystats
+                return res.status(200).send({ count_mutant_dna : numberOfMutants, count_human_dna: numberOfHumans, ratio: percentageMutants })
+      })
     })
   } catch (error) {
     console.error(error)
@@ -99,4 +121,5 @@ function registerMutant(mutant_dna){
 
 module.exports = {
   verificatorDNA,
+  readStats,
 };
--- src/routes/routing.js
+++ src/routes/routing.js
@@ -1,11 +1,12 @@
-'use strict'
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
-const readDNAController = require('../controllers/readDNAController');
+const readDNAController = require("../controllers/readDNAController");
 
 const api = express.Router();
 
-api.post('/mutant', readDNAController.verificatorDNA );
+api.post("/mutant", readDNAController.verificatorDNA);
+api.get("/stats", readDNAController.readStats);
 
-module.exports = api
+module.exports = api;
